12f639f59883caabbb89fcaf75de5bf94a0c825b
Muifrend <andrzejzadaniadomowe1@gmail.com>
2022-04-04 20:16:15 +0200

    building contact page

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.getElementsByClassName("hamburger");
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    soso.style.display = 'inline';
+  } else {
+    menu.classList.add("showMenu");
+    soso.style.display = 'none';
+  }
+}
+hamburger[0].addEventListener("click", toggleMenu);
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
 const soso = document.getElementById("face")
-let i = 45;
+let i = 0.125;
 soso.addEventListener("click", function () {
-    soso.style.transform = 'rotate('+i+'deg)';
-    if (i<360) {
-      i+=45;
-    }
-    else {
-      i-=315;
-    }
+    soso.style.transform = 'rotate('+i+'turn)';
+      i+=0.125;
   }
 )
